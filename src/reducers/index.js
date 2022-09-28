b4987230d444374cb713439a39f49ee3722edf20
unknown <fredericreact@gmail.com>
2022-09-28 17:53:02 +0800

    add reducer to have a state to use

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 
-import counter from './counter';
-
+import messages from './messages';
 export default combinerReducers({
-    counter,
+    messages,
 });
---
+++ src/reducers/messages.js
@@ -0,0 +1,36 @@
+
+
+const stateInitial = {
+    list:[
+        [
+            {
+              id:'1',
+              author:'ben',
+              content:'hello',
+            },
+            {
+              id:'2',
+              author:'fred',
+              content:'salut',
+            },
+            {
+              id:'3',
+              author:'John',
+              content:'hi',
+            },
+          ]
+    ],
+    messageText:"je suis en cours de",
+}
+
+const reducerMessage = (state = stateInitial,action ={}) => {
+
+switch(action.type){
+    default: 
+    return state;
+}
+
+}
+
+
+export default reducerMessage;
